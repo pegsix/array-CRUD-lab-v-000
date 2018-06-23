7d7dab509c69684e0d34703ea35bf1a772d1bd96
Peggy Lee <peggyleesix@gmail.com>
2018-06-23 21:25:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,25 +12,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+  ladies.unshift("Mary J. Blige")
 end
 
 def remove_element_from_end_of_array(array)
-  element = array.pop
+  "Kelis" = ladies.pop
 end
 
 def remove_element_from_start_of_array(array)
-  element = array.shift
+  "Mary J. Blige"= ladies.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  ladies(2)
 end
 
 def retrieve_first_element_from_array(array)
-
+  ladies.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  ladies.last
 end

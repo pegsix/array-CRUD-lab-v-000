b457d8ba1efbc199ec850c1686d0e24f78404748
Peggy Lee <peggyleesix@gmail.com>
2018-06-23 21:41:22 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  an_empty_array = []
+ []
 end
 
 def create_an_array
-  create_an_array = Array.new
+  ["Cardi B", "Beyonce", "Solange", "SZA"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  element = array.pop
-end
+   array.pop
+  end
 
 def remove_element_from_start_of_array(array)
-  element = array.shift
-end
+   array.shift
+  end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.last
 end

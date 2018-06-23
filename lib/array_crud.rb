94f937cb4148c40737d18197ed128e5dcd72b529
Peggy Lee <peggyleesix@gmail.com>
2018-06-23 21:34:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,10 +16,12 @@ end
 
 def remove_element_from_end_of_array(array)
   "Kelis" = Ladies.pop
+  end
 end
 
 def remove_element_from_start_of_array(array)
   "Mary J. Blige"= Ladies.shift
+  end
 end
 
 def retrieve_element_from_index(array, index_number)
